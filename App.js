f2ee81fa97aefb3336b90b3944aaa68a70a08736
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-01-28 20:10:25 +0200

    NavigationContainer

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { LoginScreen } from './Screens/LoginScreen';
 import { RegistrationScreen } from './Screens/RegistrationScreen';
-
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <>
-      {/* <LoginScreen /> */}
-      <RegistrationScreen />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Registration"
+          component={RegistrationScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
