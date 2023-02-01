8b283da60c8af99095b1fb5ee4d839d32dbffd96
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-02-01 03:54:57 +0200

    PostsScreen

--- Screens/mainScreen/Home.js
+++ Screens/mainScreen/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
 import PostsScreen from './PostsScreen';
 import CreateScreen from './CreatePostsScreen';
 import ProfileScreen from './ProfileScreen';
@@ -9,70 +8,60 @@ import { AntDesign } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 const MainTab = createBottomTabNavigator();
 
-const Home = ({navigation}) => {
-    return (
-      <MainTab.Navigator screenOptions={{ showLabel: false }}>
-        <MainTab.Screen
-          options={{
-            tabBarIcon: ({ focused, size, color }) => (
-              <MaterialCommunityIcons
-                name="postage-stamp"
-                size={size}
-                color={color}
-              />
-            ),
-            headerRight: ({ focused, size, color }) => (
-              <MaterialIcons
-                name="logout"
-                size={24}
-                color="grey"
-                onPress={() => navigation.navigate('Login')}
-              />
-            ),
-            headerRightContainerStyle: {
-              paddingRight: 20,
-            },
-          }}
-          name="Posts"
-          component={PostsScreen}
-        />
-        <MainTab.Screen
-          options={{
-            tabBarIcon: ({ focused, size, color }) => (
-              <AntDesign name="pluscircleo" size={size} color={color} />
-            ),
-          }}
-          name="Create"
-          component={CreateScreen}
-        />
-        <MainTab.Screen
-          options={{
-            tabBarIcon: ({ focused, size, color }) => (
-              <MaterialCommunityIcons
-                name="face-man-profile"
-                size={size}
-                color={color}
-              />
-            ),
-          }}
-          name="Profile"
-          component={ProfileScreen}
-        />
-      </MainTab.Navigator>
-    );
+const Home = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-    </View>
+    <MainTab.Navigator screenOptions={{ showLabel: false }}>
+      <MainTab.Screen
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <MaterialCommunityIcons
+              name="postage-stamp"
+              size={size}
+              color={color}
+            />
+          ),
+          headerRight: ({ focused, size, color }) => (
+            <MaterialIcons
+              name="logout"
+              size={24}
+              color="grey"
+              onPress={() => navigation.navigate('Login')}
+            />
+          ),
+          headerRightContainerStyle: {
+            paddingRight: 20,
+          },
+          headerTitleAlign: 'center',
+        }}
+        name="Posts"
+        component={PostsScreen}
+      />
+      <MainTab.Screen
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <AntDesign name="pluscircleo" size={size} color={color} />
+          ),
+          headerTitleAlign: 'center',
+        }}
+        name="Create"
+        component={CreateScreen}
+      />
+      <MainTab.Screen
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <MaterialCommunityIcons
+              name="face-man-profile"
+              size={size}
+              color={color}
+            />
+          ),
+          headerTitleAlign: 'center',
+        }}
+        name="Profile"
+        component={ProfileScreen}
+      />
+    </MainTab.Navigator>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default Home;
--- Screens/mainScreen/PostsScreen.js
+++ Screens/mainScreen/PostsScreen.js
@@ -1,11 +1,19 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 
-const PostsScreen = ({ navigation }) => {
-
+const PostsScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>PostsScreen</Text>
+      <View style={styles.PostsScreenUserOuter}>
+        <ImageBackground
+          style={styles.PostsScreenUserPhoto}
+          source={require('../../assets/user.png')}
+        ></ImageBackground>
+        <View style={styles.PostsScreenUserContacts}>
+          <Text style={styles.PostsScreenUserName}>Natali Romanova</Text>
+          <Text style={styles.PostsScreenUserEmail}>email@example.com</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -13,8 +21,33 @@ const PostsScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingHorizontal: 16,
+    paddingTop: 32,
+  },
+  PostsScreenUserOuter: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  PostsScreenUserPhoto: {
+    width: 60,
+    height: 60,
+    resizeMode: 'cover',
+  },
+  PostsScreenUserName: {
+    fontFamily: 'RobotoItalic',
+    fontStyle: 'Bold',
+    fontSize: 13,
+    lineHeight: 15,
+    marginLeft: 8,
+    color: '#212121',
+  },
+  PostsScreenUserEmail: {
+    fontFamily: 'RobotoItalic',
+    fontStyle: 'Regular',
+    fontSize: 11,
+    lineHeight: 13,
+    marginLeft: 8,
+    color: '#212121CC',
   },
 });
 
