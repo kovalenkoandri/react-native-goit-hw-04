217451f0bbc7ea46696f96422fb90afab93dab00
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-02-01 23:38:10 +0200

    focusFlag

--- Screens/mainScreen/Home.js
+++ Screens/mainScreen/Home.js
@@ -7,17 +7,26 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 import { View, Image, Text } from 'react-native';
-import SvgCreate from '../../helpers/SvgCreate';
+import SvgCreateFocused from '../../helpers/SvgCreateFocused';
+import SvgCreateUnfocused from '../../helpers/SvgCreateUnfocused';
 import SvgPosts from '../../helpers/SvgPosts';
-import SvgProfile from '../../helpers/SvgProfile';
+import SvgProfileFocused from '../../helpers/SvgProfileFocused';
+import SvgProfileUnfocused from '../../helpers/SvgProfileUnfocused';
+
 const MainTab = createBottomTabNavigator();
 
 const Home = ({ navigation }) => {
+  let focusFlag = false;
   return (
     <MainTab.Navigator screenOptions={{ tabBarShowLabel: false }}>
       <MainTab.Screen
         options={{
-          tabBarIcon: ({ focused, size, color }) => <SvgPosts />,
+          tabBarIcon: ({ focused, size, color }) => {
+            if (focused === true) {
+              focusFlag = true;
+            }
+            return <SvgPosts />;
+          },
           headerRight: ({ focused, size, color }) => (
             <MaterialIcons
               name="logout"
@@ -36,7 +45,14 @@ const Home = ({ navigation }) => {
       />
       <MainTab.Screen
         options={{
-          tabBarIcon: ({ focused, size, color }) => <SvgCreate />,
+          tabBarIcon: ({ focused, size, color }) => {
+            if (focusFlag === true || focused === true) {
+              console.log(`focusFlag ${focusFlag}`);
+              console.log(`focused ${focused}`);
+              return <SvgCreateFocused />;
+            }
+            else return <SvgCreateUnfocused />;
+          },
           headerTitleAlign: 'center',
         }}
         name="Создать публикацию"
@@ -44,7 +60,14 @@ const Home = ({ navigation }) => {
       />
       <MainTab.Screen
         options={{
-          tabBarIcon: ({ focused, size, color }) => <SvgProfile />,
+          tabBarIcon: ({ focused, size, color }) => {
+            if (focused === true) {
+              focusFlag = false;
+              console.log(focusFlag);
+              return <SvgProfileFocused />;
+            }
+            return <SvgProfileUnfocused />;
+          },
           headerTitleAlign: 'center',
         }}
         name="Профиль"
--- helpers/SvgProfileUnfocused.js
+++ helpers/SvgProfileUnfocused.js
@@ -1,13 +1,8 @@
 import * as React from 'react';
 import Svg, { Path } from 'react-native-svg';
 
-const SvgProfile = () => (
-  <Svg
-    width={40}
-    height={40}
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
+const SvgProfileUnfocused = () => (
+  <Svg width={40} height={40} fill="none" xmlns="http://www.w3.org/2000/svg">
     <Path
       d="M28 29v-2a4 4 0 0 0-4-4h-8a4 4 0 0 0-4 4v2"
       stroke="#212121"
@@ -26,4 +21,4 @@ const SvgProfile = () => (
   </Svg>
 );
 
-export default SvgProfile;
+export default SvgProfileUnfocused;
